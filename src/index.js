9cf7689ff70ee440e2633c3d5b1845688e48ceeb
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-13 22:56:51 +0200

    refactoring App component to class component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-// Create a web component. This component should produce some html
-// This component is a function
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyD4JxQgebX6T1d1PnwShMY0yWmpWcAs50c';
 
-const App = function() {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'surfboards' }, videos => this.setState({ videos }));
+	}
+
+    render() {
+
+        return ( <div>
+            <SearchBar />
+            </div>
+        );
+
+    }
 }
 
 
-// Put this component in the DOM
-
-ReactDom.render(
-	<App />,
-	document.querySelector('.container')
+ReactDom.render( < App / > ,
+    document.querySelector('.container')
 );
