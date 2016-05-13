683d38eaa225e97bb9debf607e0e42535267c3eb
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-05-13 21:35:02 +0200

    add constructor function and state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { term: ''};
+	}
 	render() {
-		return <input onChange={evt => console.log(evt.target.value)} />;
+		return (
+			<div>
+			<input onChange={evt => this.setState({ term: evt.target.value})} />
+			</div>
+			);
 	}
 
 }
